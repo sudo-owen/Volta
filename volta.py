f7a5e452fd7d8ed1d992ce343c78367296c7fd78
Owen <owenshen24@gmail.com>
2018-09-05 17:54:04 -0700

    Added more to the script + templating

--- volta.py
+++ volta.py
@@ -5,34 +5,45 @@ from datetime import datetime
 from jinja2 import Environment, PackageLoader
 from markdown2 import markdown
 
+# JSON of all blog posts
 POSTS = {}
-LAST_MODIFIED = 0
 
-# Check the last time the script was run
-# Update the metadata for time
-with open('METADATA.json', 'r+') as f:
-	data = json.load(f)
-	LAST_MODIFIED = data['last-modified']
-	data['last-modified'] = int(time.time())
-	f.seek(0)
-	json.dump(data, f)
-	f.truncate()
-	f.close()
-
-
-# Go through all posts on the 'content' directory and parse those who have
-# been updated since the last time the script was run
+# Go through all posts and populate POSTS.
 for md_post in os.listdir('content'):
 	file_path = os.path.join('content', md_post)
-	last_modified_time = os.path.getmtime(file_path)
 
-	if (True): #last_modified_time > LAST_MODIFIED):
-		with open(file_path, 'r') as file:
-			POSTS[file_path] = markdown(file.read(), extras=['metadata'])
-			
-			# Automatically set the last modified time
-			POSTS[file_path].metadata['date'] = last_modified_time
-			POSTS[file_path].metadata['date-string'] = datetime.fromtimestamp(last_modified_time).strftime('%Y-%m-%d %H:%M:%S')
+	# Grab the date last modified.
+	time = int(os.path.getmtime(file_path))
+	last_modified_time = datetime.fromtimestamp(time).strftime(
+		'%Y-%m-%d %H:%M:%S')
 
-for k in POSTS.keys():
-	print(POSTS[k].metadata)
+	# Open and parse the file.
+	with open(file_path, 'r') as file:
+		parsed_file = markdown(file.read(), extras=['metadata'])
+		title = parsed_file.metadata['title']
+		anchor = title.replace(' ', '-')
+		
+		data = {
+			'content': parsed_file,
+			'title': title,
+			'date': last_modified_time,
+			'timestamp': time,
+			'anchor': anchor
+		}
+
+		POSTS[anchor] = data
+
+# Order posts from newest to oldest
+POSTS = {
+	post : POSTS[post] for post in sorted(POSTS,
+		key=lambda post: POSTS[post]['timestamp'],
+		reverse=True)
+}
+
+# Render post in Jinja2
+env = Environment(loader=PackageLoader('volta', 'templates'))
+index_template = env.get_template('index.html')
+index_html_content = index_template.render(posts = 
+	[POSTS[post] for post in POSTS])
+
+print(index_html_content)
